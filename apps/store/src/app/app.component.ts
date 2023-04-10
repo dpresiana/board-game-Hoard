118b093df8da482e26b1ab683a1e89b5a9e04aac
dpresiana <dpresiana@vmware.com>
2023-04-10 10:47:41 +0300

    Lab 2 - adding @nrwl/angular plugin
    adding @angular/material @angular/cdk

---
+++ apps/store/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { getAllGames } from '../fake-api';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'bg-hoard-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [MatCardModule, CommonModule]
+})
+export class AppComponent {
+  title = 'Board Game Hoard';
+  games = getAllGames();
+}
